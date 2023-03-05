e16cebbd48d639ae31dd6b1aa2d66bd0f6b57b4c
Vidyadhare <vidyadharek@gmail.com>
2023-03-05 16:39:58 +0530

    adding the new springboot roject

---
+++ src/main/java/com/PracticeMarch2023/demoSampleSpringboot/DemoSampleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.PracticeMarch2023.demoSampleSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSampleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSampleSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticeMarch2023/demoSampleSpringboot/DemoSampleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticeMarch2023.demoSampleSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSampleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
